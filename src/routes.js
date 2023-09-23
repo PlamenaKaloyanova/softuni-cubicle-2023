95345d6877c4f05413b46dd138847a0b5f9a556f
PlamenaKaloyanova <plamy_90@abv.bg>
2023-09-23 09:42:59 +0300

    Add routes

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+const cubeController=require('./controllers/cubeController')
+const router=require('express').Router();
+
+
+router.get('/',(req,res)=>{
+    res.render('index')
+});
+router.get('/about',(req,res)=>{
+    res.render('about')
+});
+
+// app.get('/create',(req,res)=>{
+//     res.render('create')
+// })
+
+router.get('/create',cubeController.getCreateCub);
+
+module.exports=router;
